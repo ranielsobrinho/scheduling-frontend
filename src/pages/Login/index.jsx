36a8eb70acc2e75b3cb12eec3eebf049425f6fe3
Raniel Sobrinho <raniel.sobrinho@g4flex.com.br>
2025-01-07 11:05:43 -0300

    feat: create login page component

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,34 @@
+import { Grid2 as Grid, CssBaseline } from "@mui/material";
+import SeuCarlosBackground from "../../assets/seu-carlos.png";
+import LoginForm from "../../components/LoginForm";
+
+const Login = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Grid container spacing={1}>
+        <Grid item size={8}>
+          <img
+            src={SeuCarlosBackground}
+            alt="Seu Carlos's image"
+            style={{ height: "99.9vh", width: "60vw" }}
+          />
+        </Grid>
+        <Grid
+          item
+          size={4}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#A5A5A5",
+          }}
+        >
+          <LoginForm />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Login;
